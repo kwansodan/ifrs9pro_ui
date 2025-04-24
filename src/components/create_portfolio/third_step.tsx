0750b0cacdb1030a7c8a87f6e2d1dd0d24650760
Psikwe <Psikwe07@gmail.com>
2025-04-24 23:14:07 +0000

    remove object in payload

--- src/components/create_portfolio/third_step.tsx
+++ src/components/create_portfolio/third_step.tsx
@@ -75,11 +75,11 @@ function ThirdStep({ close, id, setStep }: any) {
       setIsCreating(false);
       return;
     }
-    const finalPayload = {
-      ecl_staging_config: payload,
-    };
+    // const finalPayload = {
+    //   ecl_staging_config: payload,
+    // };
     try {
-      CreateSecondStepPortfolioApi(id, finalPayload)
+      CreateSecondStepPortfolioApi(id, payload)
         .then((res) => {
           setIsCreating(false);
           if (res.status === 200 || res.status === 201) {
